9a34fca5c63cfce765550396e5f6123c1a75c49b
Landon Manning <LManning17@gmail.com>
2014-01-16 02:26:26 -0400

    Fixed Custom Layer opacity

--- sti.lua
+++ sti.lua
@@ -188,7 +188,9 @@ function Map:drawImageLayer(layer)
 end
 
 function Map:drawCustomLayer(layer)
+	love.graphics.setColor(255, 255, 255, 255 * layer.opacity)
 	layer:draw()
+	love.graphics.setColor(255, 255, 255, 255)
 end
 
 function Map:drawCollisionMap()
