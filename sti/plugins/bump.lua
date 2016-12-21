654051bf0e310fb0bf436fef77845f2db3a66e00
Daniel Power <me@danielpower.ca>
2016-12-21 15:31:29 -0330

    Added support for collision objectGroups

--- sti/plugins/bump.lua
+++ sti/plugins/bump.lua
@@ -25,9 +25,28 @@ return {
 				-- Every object in every instance of a tile
 				if tile.properties and tile.properties.collidable == true and map.tileInstances[gid] then
 					for _, instance in ipairs(map.tileInstances[gid]) do
-						local t = {properties = tile.properties, x = instance.x + map.offsetx, y = instance.y + map.offsety, width = map.tilewidth, height = map.tileheight, layer = instance.layer }
-						world:add(t,  t.x,t.y, t.width,t.height)
-						table.insert(collidables,t)
+						local colBox = false
+						if tile.objectGroup and tile.objectGroup.objects then
+							for _, object in ipairs(tile.objectGroup.objects) do
+								if object.type == 'colBox' then
+									colBox = true
+									local t = {}
+									t.properties = tile.properties
+									t.x = instance.x + map.offsetx + object.x
+									t.y = instance.y + map.offsety + object.y
+									t.width = object.width
+									t.height = object.height
+									t.layer = instance.layer
+									world:add(t, t.x, t.y, object.width, object.height)
+									table.insert(collidables, t)
+								end
+							end
+						end
+						if colBox == false then
+							local t = { properties = tile.properties, x = instance.x + map.offsetx, y = instance.y + map.offsety, width = map.tilewidth, height = map.tileheight, layer = instance.layer }
+							world:add(t,  t.x,t.y, t.width,t.height)
+							table.insert(collidables,t)
+						end
 					end
 				end
 			end
@@ -35,13 +54,32 @@ return {
 
 		for _, layer in ipairs(map.layers) do
 			-- Entire layer
-			if layer.properties.collidable == true then
+			if layer.properties.collidable == "true" then
 				if layer.type == "tilelayer" then
 					for y, tiles in ipairs(layer.data) do
 						for x, tile in pairs(tiles) do
-							local t = {properties = tile.properties, x = (x - 1) * map.tilewidth + tile.offset.x + map.offsetx, y = (y - 1) * map.tileheight + tile.offset.y + map.offsety, width = tile.width, height = tile.height, layer = layer }
-							world:add(t, t.x,t.y, t.width,t.height )
-							table.insert(collidables,t)
+							local colBox = false
+							if tile.objectGroup and tile.objectGroup.objects then
+								for _, object in ipairs(tile.objectGroup.objects) do
+									if object.type == 'colBox' then
+										colBox = true
+										local t = {}
+										t.properties = tile.properties
+										t.x = ((x-1) * map.tilewidth + tile.offset.x + map.offsetx) + object.x
+										t.y = ((y-1) * map.tileheight + tile.offset.y + map.offsety) + object.y
+										t.width = object.width
+										t.height = object.height
+										t.layer = layer
+										world:add(t, t.x, t.y, object.width, object.height)
+										table.insert(collidables, t)
+									end
+								end
+							end
+							if colBox == false then
+								local t = {properties = tile.properties, x = (x - 1) * map.tilewidth + tile.offset.x + map.offsetx, y = (y - 1) * map.tileheight + tile.offset.y + map.offsety, width = tile.width, height = tile.height, layer = layer }
+								world:add(t, t.x,t.y, t.width,t.height )
+								table.insert(collidables,t)
+							end
 						end
 					end
 				elseif layer.type == "imagelayer" then
