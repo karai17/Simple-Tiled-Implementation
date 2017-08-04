9caae9347df62e4b3049e4e80a3f575f9df6f075
Jan <jackflash5500@gmail.com>
2017-08-04 21:09:03 +0200

    Fix error when tx, ty not specified in box2d_draw
    Same issue as with https://github.com/karai17/Simple-Tiled-Implementation/commit/6e5bf81a996f0507fb7c2b28ab2a79a2a64a3c74

--- sti/plugins/bump.lua
+++ sti/plugins/bump.lua
@@ -173,7 +173,7 @@ return {
 	bump_draw = function(map, world, tx, ty, sx, sy)
 		lg.push()
 		lg.scale(sx or 1, sy or sx or 1)
-		lg.translate(math.floor(tx) or 0, math.floor(ty) or 0)
+		lg.translate(math.floor(tx or 0), math.floor(ty or 0))
 
 		for _, collidable in pairs(map.bump_collidables) do
 			lg.rectangle("line", world:getRect(collidable))
